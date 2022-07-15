84d6f1e393869ec7f2f30e6a699364a38a30cc49
Christian Skorobogatow <christiansko@icloud.com>
2022-07-15 08:40:20 +0200

    Initial Commit

---
+++ Escaping/EscapingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapingApp.swift
+//  Escaping
+//
+//  Created by Christian Skorobogatow on 15/7/22.
+//
+
+import SwiftUI
+
+@main
+struct EscapingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
