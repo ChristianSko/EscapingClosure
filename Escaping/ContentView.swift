28ac1733cf8d4520bd57ff166613c7ddcf8752b4
Christian Skorobogatow <christiansko@icloud.com>
2022-07-15 09:03:07 +0200

    feat(): escaping closures examples

--- Escaping/ContentView.swift
+++ Escaping/ContentView.swift
@@ -7,10 +7,65 @@
 
 import SwiftUI
 
+class EscapingViewModel: ObservableObject {
+    @Published var text: String = "Hello"
+
+    
+    func getData() {
+        downloadData5 { [weak self] returnedResult in
+            self?.text = returnedResult.data
+        }
+    }
+    
+    func downloadData() -> String {
+        return "New data!"
+    }
+    
+    func downloadData2(completionHandler: (_ data: String) -> Void) {
+        completionHandler("New data!")
+    }
+    
+    func downloadData3(completionHandler: @escaping (_ data: String) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completionHandler("New data!")
+        }
+    }
+    
+    func downloadData4(completionHandler: @escaping (DownloadResult) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let result = DownloadResult(data: "New data!")
+            completionHandler(result)
+        }
+    }
+    
+    func downloadData5(completionHandler: @escaping DownloadCompletion) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let result = DownloadResult(data: "New data!")
+            completionHandler(result)
+        }
+    }
+    
+ 
+}
+
+typealias DownloadCompletion = (DownloadResult) -> ()
+
+struct DownloadResult {
+    let data: String
+}
+
 struct ContentView: View {
+    
+    @StateObject var vm = EscapingViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(vm.text)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundColor(.blue)
+            .onTapGesture {
+                vm.getData()
+            }
     }
 }
 
